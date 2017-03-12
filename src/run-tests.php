7ba42c5b90cf78161b4b1b92ada670b7384d8afb
Jaroslav Vaculik -IJVo <ijvo.ijvo@gmail.com>
2017-03-12 23:57:43 +0100

    run-tests.php before 'echo' for tape output

---
+++ src/run-tests.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '/Runner.php';
+$runner = new \Testbench\Runner;
+$vendorDir = $runner->findVendorDirectory();
+$testsDir = dirname($vendorDir) . '/tests';
+
+require $vendorDir . '/autoload.php';
+$script = array_shift($_SERVER['argv']);
+$_SERVER['argv'] = $runner->prepareArguments($_SERVER['argv'], $testsDir);
+array_unshift($_SERVER['argv'], $script);
+
+// echo "TESTBENCH edition\n";
+require $vendorDir . '/nette/tester/src/tester.php';
